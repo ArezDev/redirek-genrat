b73c5d374cceddc1270a236bb5897db253fbfe7b
ArezDev <arezzdev@gmail.com>
2025-06-01 21:07:38 +0700

    modified:   pages/api/redirect/click.ts

--- pages/api/redirect/click.ts
+++ pages/api/redirect/click.ts
@@ -180,7 +180,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     return res.status(200).json({
-      data: encoded,
+      clickId: encoded,
       status: 'OK',
       message: 'Click successfully tracked!',
     });
