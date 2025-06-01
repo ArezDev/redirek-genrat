a0bd3027e541f7e11ce9357e2f3a1b1f023544ad
ArezDev <arezzdev@gmail.com>
2025-06-01 20:48:45 +0700

    modified:   app/[...slug]/page.tsx 	modified:   app/api/click/route.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   utils/firebaseAdmin.ts

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -90,12 +90,17 @@ export default function Link() {
           return;
         }
 
-        console.log(matchedNetwork)
+        //console.log(matchedNetwork)
 
         setNetworkData(JSON.stringify(matchedNetwork));
         setStatusText('Creating click record...');
 
-        const create_clicks = await axios.post(`/api/click?sub=${sub}&network=${networkCode}`);
+        const create_clicks = await axios.get(`/api/click`,{
+          params:{
+            sub: sub,
+            network: networkCode
+          }
+        });
         if (create_clicks.status !== 200) {
           setStatusText('Failed to create click record.');
           return;
