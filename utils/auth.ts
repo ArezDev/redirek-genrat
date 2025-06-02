6f3d437501b45b2963e0a50747b6741a7d219221
ArezDev <arezzdev@gmail.com>
2025-06-02 10:56:58 +0700

    modified:   utils/auth.ts

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,18 +1,16 @@
 // lib/auth.ts
-import { db } from "./firebase";
+import { db } from "./firebaseAdmin";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import bcrypt from "bcryptjs";
 
 export const verifyLogin = async (username: string, password: string) => {
-  const usersRef = collection(db, "users");
-  const q = query(usersRef, where("username", "==", username), where("role", "==", "ketua"));
-  const querySnapshot = await getDocs(q);
-
-  if (querySnapshot.empty) {
+  const usersRef = await db.collection("users")
+      .where("username", "==", username).where("role", "==", "ketua").get();
+  if (usersRef.empty) {
     throw new Error("User not found or not ketua");
   }
 
-  const userDoc = querySnapshot.docs[0];
+  const userDoc = usersRef.docs[0];
   const userData = userDoc.data();
 
   const isPasswordValid = await bcrypt.compare(password, userData.password);
